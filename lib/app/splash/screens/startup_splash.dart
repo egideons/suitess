0c1f61fd7ead525562aacf8a0704d9225a5ff2ed
egideons <egideonchuks@gmail.com>
2024-04-17 15:20:17 +0100

    added an auth controller

--- lib/app/splash/screens/startup_splash.dart
+++ lib/app/splash/screens/startup_splash.dart
@@ -4,8 +4,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kribb/app/onboarding/screens/onboarding.dart';
 
+import '../../../src/controllers/auth_controller.dart';
 import '../widgets/android/android_scaffold.dart';
 import '../widgets/ios/cupertino_page_scaffold.dart';
 
@@ -16,24 +16,19 @@ class StartupSplashscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
 
-    Future.delayed(
-      const Duration(seconds: 3),
-      () async {
-        await Get.offAll(
-          () => const Onboarding(),
-          routeName: "onboarding",
-          fullscreenDialog: true,
-          curve: Curves.easeInOut,
-          predicate: (routes) => false,
-          popGesture: false,
-          duration: const Duration(milliseconds: 500),
-          transition: Get.defaultTransition,
-        );
+    if (Platform.isIOS) {
+      return GetBuilder<AuthController>(
+        init: AuthController(),
+        builder: (controller) {
+          return IOSScaffold(media: media);
+        },
+      );
+    }
+    return GetBuilder<AuthController>(
+      init: AuthController(),
+      builder: (controller) {
+        return AndroidScaffold(media: media);
       },
     );
-    if (Platform.isIOS) {
-      return IOSScaffold(media: media);
-    }
-    return AndroidScaffold(media: media);
   }
 }
---
+++ lib/src/controllers/auth_controller.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../app/onboarding/screens/onboarding.dart';
+
+class AuthController extends GetxController {
+  static AuthController get instance {
+    return Get.find<AuthController>();
+  }
+
+  var isLoading = false.obs;
+  var isResendingOTP = false.obs;
+
+  var responseStatus = 0.obs;
+  var responseMessage = "".obs;
+
+  @override
+  void onInit() {
+    Timer(const Duration(seconds: 2), () async {
+      await Get.offAll(
+        () => const Onboarding(),
+        routeName: "onboarding",
+        fullscreenDialog: true,
+        curve: Curves.easeInOut,
+        predicate: (routes) => false,
+        popGesture: false,
+        duration: const Duration(milliseconds: 500),
+        transition: Get.defaultTransition,
+      );
+    });
+    super.onInit();
+  }
+}
