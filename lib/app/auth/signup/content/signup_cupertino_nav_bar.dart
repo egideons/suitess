00babe17c26561b6d67d3eda02d40e84dddb2eab
egideons <egideonchuks@gmail.com>
2024-04-19 02:24:15 +0100

    changed file location of the signup cupertino nav bar

---
+++ lib/app/auth/signup/content/signup_cupertino_nav_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+
+import '../../../../src/constants/consts.dart';
+import '../../../../theme/colors.dart';
+
+ObstructingPreferredSizeWidget signupCupertinoNavBar({
+  colorScheme,
+  media,
+}) {
+  return CupertinoNavigationBar(
+    transitionBetweenRoutes: true,
+    backgroundColor: colorScheme.background,
+    border: Border.all(color: kTransparentColor),
+    middle: Container(
+      width: media.width - 250,
+      padding: const EdgeInsets.all(10),
+      decoration: ShapeDecoration(
+        color: Get.isDarkMode
+            ? kSuccessColor.withOpacity(0.2)
+            : kSuccessColor.withOpacity(0.06),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FaIcon(
+              FontAwesomeIcons.key,
+              color: kSuccessColor,
+              size: 12,
+            ),
+            kHalfWidthSizedBox,
+            Text(
+              "Create account",
+              style: defaultTextStyle(color: kSuccessColor),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
--- lib/app/auth/signup/widgets/ios/signup_cupertino_scaffold.dart
+++ lib/app/auth/signup/widgets/ios/signup_cupertino_scaffold.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
+import 'package:kribb/app/auth/signup/content/signup_cupertino_nav_bar.dart';
 import 'package:kribb/app/auth/signup/content/signup_page_header.dart';
-
-import '../../../../../src/constants/consts.dart';
-import '../../../../../theme/colors.dart';
 
 class SignupCupertinoScaffold extends StatelessWidget {
   const SignupCupertinoScaffold({super.key});
@@ -16,41 +12,8 @@ class SignupCupertinoScaffold extends StatelessWidget {
     var colorScheme = Theme.of(context).colorScheme;
 
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        transitionBetweenRoutes: true,
-        backgroundColor: colorScheme.background,
-        border: Border.all(color: kTransparentColor),
-        middle: Container(
-          width: media.width - 250,
-          padding: const EdgeInsets.all(10),
-          decoration: ShapeDecoration(
-            color: Get.isDarkMode
-                ? kSuccessColor.withOpacity(0.2)
-                : kSuccessColor.withOpacity(0.06),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
-            ),
-          ),
-          child: Center(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FaIcon(
-                  FontAwesomeIcons.key,
-                  color: kSuccessColor,
-                  size: 12,
-                ),
-                kHalfWidthSizedBox,
-                Text(
-                  "Create account",
-                  style: defaultTextStyle(color: kSuccessColor),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      navigationBar:
+          signupCupertinoNavBar(colorScheme: colorScheme, media: media),
       child: SafeArea(
         child: ListView(
           children: [
