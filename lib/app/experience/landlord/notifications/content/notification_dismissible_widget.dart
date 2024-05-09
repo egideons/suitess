b5fa6c44dfe627065309bd7c5c0f7da104f234b9
egideons <egideonchuks@gmail.com>
2024-05-09 16:10:34 +0100

    finished ui iteration for the notifications screen

---
+++ lib/app/experience/landlord/notifications/content/notification_dismissible_widget.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
+
+import '../../../../../src/constants/consts.dart';
+import '../../../../../theme/colors.dart';
+
+notificationDismissibleWidget(
+  colorScheme,
+  media, {
+  onDismissed,
+  notificationColor,
+  notificationMessage,
+  notificationTime,
+}) {
+  return Dismissible(
+    key: GlobalKey(),
+    direction: DismissDirection.endToStart,
+    onDismissed: onDismissed ?? (direction) {},
+    background: Container(
+      decoration: BoxDecoration(color: colorScheme.background),
+      width: media.width,
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: ShapeDecoration(
+            color: kRedColor,
+            shape: const CircleBorder(),
+          ),
+          child: Icon(
+            Iconsax.trash,
+            color: kLightBackgroundColor,
+          ),
+        ),
+      ),
+    ),
+    child: Container(
+      padding: const EdgeInsets.all(20),
+      width: media.width,
+      decoration: ShapeDecoration(
+        color: colorScheme.background,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        shadows: [
+          BoxShadow(
+            color: colorScheme.primary.withOpacity(0.1),
+            offset: const Offset(4, 10),
+            blurRadius: 10,
+            spreadRadius: 4,
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 8,
+            width: 8,
+            decoration: ShapeDecoration(
+              shape: const CircleBorder(),
+              color: notificationColor ?? colorScheme.inversePrimary,
+            ),
+          ),
+          kHalfWidthSizedBox,
+          Column(
+            children: [
+              SizedBox(
+                width: media.width / 1.4,
+                child: ReadMoreText(
+                  notificationMessage ?? "",
+                  trimMode: TrimMode.Line,
+                  trimLines: 4,
+                  textAlign: TextAlign.start,
+                  style: defaultTextStyle(
+                    color: colorScheme.primary,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: media.width / 1.4,
+                child: Text(
+                  notificationTime ?? "",
+                  textAlign: TextAlign.start,
+                  style: defaultTextStyle(
+                    color: colorScheme.inversePrimary,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 8,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
--- lib/app/experience/landlord/notifications/widgets/ios/landlord_notifications_cupertino_scaffold.dart
+++ lib/app/experience/landlord/notifications/widgets/ios/landlord_notifications_cupertino_scaffold.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:kribb/theme/colors.dart';
-import 'package:readmore/readmore.dart';
 
 import '../../../../../../src/constants/consts.dart';
 import '../../../../../../src/controllers/landlord_notifications_controller.dart';
 import '../../../../../../src/utils/buttons/ios/scroll_to_top_cupertino_button.dart';
+import '../../content/notification_dismissible_widget.dart';
 
 class LandLordNotificationsCupertinoScaffold
     extends GetView<LandLordNotificationsController> {
@@ -82,103 +81,6 @@ class LandLordNotificationsCupertinoScaffold
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  notificationDismissibleWidget(
-    colorScheme,
-    media, {
-    onDismissed,
-    notificationColor,
-    notificationMessage,
-    notificationTime,
-  }) {
-    return Dismissible(
-      key: GlobalKey(),
-      direction: DismissDirection.endToStart,
-      onDismissed: onDismissed ?? (direction) {},
-      background: Container(
-        decoration: BoxDecoration(color: colorScheme.background),
-        width: media.width,
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            decoration: ShapeDecoration(
-              color: kRedColor,
-              shape: const CircleBorder(),
-            ),
-            child: Icon(
-              Iconsax.trash,
-              color: kLightBackgroundColor,
-            ),
-          ),
-        ),
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        width: media.width,
-        decoration: ShapeDecoration(
-          color: colorScheme.background,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          shadows: [
-            BoxShadow(
-              color: colorScheme.primary.withOpacity(0.1),
-              offset: const Offset(4, 10),
-              blurRadius: 10,
-              spreadRadius: 4,
-            ),
-          ],
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 8,
-              width: 8,
-              decoration: ShapeDecoration(
-                shape: const CircleBorder(),
-                color: notificationColor ?? colorScheme.inversePrimary,
-              ),
-            ),
-            kHalfWidthSizedBox,
-            Column(
-              children: [
-                SizedBox(
-                  width: media.width / 1.4,
-                  child: ReadMoreText(
-                    notificationMessage ?? "",
-                    trimMode: TrimMode.Line,
-                    trimLines: 4,
-                    textAlign: TextAlign.start,
-                    style: defaultTextStyle(
-                      color: colorScheme.primary,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                SizedBox(
-                  width: media.width / 1.4,
-                  child: Text(
-                    notificationTime ?? "",
-                    textAlign: TextAlign.start,
-                    style: defaultTextStyle(
-                      color: colorScheme.inversePrimary,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 8,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
