cadc38fc363e18df81dcdb7fd26498e3218da8d3
egideons <egideonchuks@gmail.com>
2024-06-20 03:39:25 +0100

    continuing iteration on the notifications screen

---
+++ lib/app/screens/notifications/widgets/android/components/app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import '../../../../../../src/constants/assets.dart';
+import '../../../../../../src/constants/consts.dart';
+
+appBar(ColorScheme colorScheme, Size media, {Function()? goToSettings}) {
+  return AppBar(
+    backgroundColor: colorScheme.surface,
+    centerTitle: true,
+    leading: IconButton(
+      onPressed: () {
+        Get.back();
+      },
+      icon: Icon(Icons.chevron_left, color: colorScheme.primary),
+    ),
+    title: SizedBox(
+      width: media.width / 2,
+      child: Text(
+        "Notifications",
+        textAlign: TextAlign.center,
+        style: defaultTextStyle(
+          color: colorScheme.primary,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
+    actions: [
+      IconButton(
+        onPressed: goToSettings ?? () {},
+        icon: SvgPicture.asset(
+          Assets.settingsOutline,
+          fit: BoxFit.contain,
+        ),
+      ),
+    ],
+  );
+}
--- lib/app/screens/notifications/widgets/android/notifications_scaffold.dart
+++ lib/app/screens/notifications/widgets/android/notifications_scaffold.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-import '../../../../../src/constants/assets.dart';
 import '../../../../../src/constants/consts.dart';
 import '../../../../../src/controllers/app/notifications_controller.dart';
 import '../../../../../theme/colors.dart';
 import '../../content/notification_dismissible_widget.dart';
+import 'components/app_bar.dart';
+import 'components/empty_notification.dart';
 
 class NotificationsScaffold extends GetView<NotificationsController> {
   const NotificationsScaffold({super.key});
@@ -37,12 +36,12 @@ class NotificationsScaffold extends GetView<NotificationsController> {
           onRefresh: controller.onRefresh,
           backgroundColor: colorScheme.surface,
           color: colorScheme.primary,
-          child: CupertinoScrollbar(
+          child: Scrollbar(
             controller: controller.scrollController,
             child: GetBuilder<NotificationsController>(
               init: NotificationsController(),
               builder: (controller) {
-                return controller.hasNoNotifications.value
+                return !controller.hasNoNotifications.value
                     ? emptyNotifications(media, colorScheme)
                     : ListView.separated(
                         controller: controller.scrollController,
@@ -67,85 +66,6 @@ class NotificationsScaffold extends GetView<NotificationsController> {
           ),
         ),
       ),
-    );
-  }
-
-  emptyNotifications(
-    Size media,
-    ColorScheme colorScheme, {
-    String? title,
-    String? message,
-  }) {
-    return Container(
-      height: media.height / 2,
-      width: media.width,
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SvgPicture.asset(
-            Assets.noNotificationsSvg,
-            height: media.height / 4,
-            fit: BoxFit.cover,
-          ),
-          kSizedBox,
-          Text(
-            title ?? "Oops",
-            textAlign: TextAlign.center,
-            style: defaultTextStyle(
-              color: colorScheme.inversePrimary,
-              fontSize: 16,
-              fontWeight: FontWeight.w800,
-            ),
-          ),
-          Text(
-            message ??
-                "You're all caught up! We'll notify you when there's something new.",
-            maxLines: 4,
-            textAlign: TextAlign.center,
-            style: defaultTextStyle(
-              color: colorScheme.inversePrimary,
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  appBar(ColorScheme colorScheme, Size media, {Function()? goToSettings}) {
-    return AppBar(
-      backgroundColor: colorScheme.surface,
-      centerTitle: true,
-      leading: IconButton(
-        onPressed: () {
-          Get.back();
-        },
-        icon: Icon(Icons.chevron_left, color: colorScheme.primary),
-      ),
-      title: SizedBox(
-        width: media.width / 2,
-        child: Text(
-          "Notifications",
-          textAlign: TextAlign.center,
-          style: defaultTextStyle(
-            color: colorScheme.primary,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ),
-      actions: [
-        IconButton(
-          onPressed: goToSettings ?? () {},
-          icon: SvgPicture.asset(
-            Assets.settingsOutline,
-            fit: BoxFit.contain,
-          ),
-        ),
-      ],
     );
   }
 }
